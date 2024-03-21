64c16e48678e77b1e7b49494ab09109256bec0a7
Raphael Leonelli <lurgouf82@hotmail.com>
2024-03-21 12:07:29 +0100

    Reformat database migration and add initial data
    Indentation of the database migration has been adjusted for better readability. Included DB to insert initial data into 'impostazione_proprietario_veicolo' table for storing vehicle owner's information.

--- database/migrations/2024_03_07_104312_create_impostazione_proprietario_veicolo_table.php
+++ database/migrations/2024_03_07_104312_create_impostazione_proprietario_veicolo_table.php
@@ -1,45 +1,45 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\DB;
+    use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('impostazione_proprietario_veicolo', function (Blueprint $table) {
-            $table->id(); // Equivalent to `bigint(20) unsigned NOT NULL AUTO_INCREMENT`
-            $table->string('nome', 256); // Equivalent to `varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL`
-            $table->timestamps(); // Creates `created_at` and `updated_at` columns
-        });
+    return new class extends Migration {
+        /**
+         * Run the migrations.
+         */
+        public function up(): void
+        {
+            Schema::create('impostazione_proprietario_veicolo', function (Blueprint $table) {
+                $table->id(); // Equivalent to `bigint(20) unsigned NOT NULL AUTO_INCREMENT`
+                $table->string('nome', 256); // Equivalent to `varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL`
+                $table->timestamps(); // Creates `created_at` and `updated_at` columns
+            });
 
-        DB::table('impostazione_proprietario_veicolo')->insert([
-            ['id' => '1' , 'nome' => 'TOP CAR' , 'created_at' => NOW() , 'updated_at' => NOW() ],
-            ['id' => '2' , 'nome' => 'AUTOMOTIVE' , 'created_at' => NOW() , 'updated_at' => NOW() ],
-            ['id' => '3' , 'nome' => 'MOVIMOB' , 'created_at' => NOW() , 'updated_at' => NOW() ],
-            ['id' => '4' , 'nome' => 'RL2' , 'created_at' => NOW() , 'updated_at' => NOW() ],
-            ['id' => '5' , 'nome' => 'VENETA LOGISTIC' , 'created_at' => NOW() , 'updated_at' => NOW() ],
-            ['id' => '6' , 'nome' => 'SOLUZIONE VEICOLARE' , 'created_at' => NOW() , 'updated_at' => NOW() ],
-            ['id' => '7' , 'nome' => 'B7' , 'created_at' => NOW() , 'updated_at' => NOW() ],
-            ['id' => '8' , 'nome' => 'GAIA78' , 'created_at' => NOW() , 'updated_at' => NOW() ],
-            ['id' => '9' , 'nome' => 'LEASEPLAN' , 'created_at' => NOW() , 'updated_at' => NOW() ],
-            ['id' => '10' , 'nome' => 'DINA BIRIIAC' , 'created_at' => NOW() , 'updated_at' => NOW() ],
-            ['id' => '11' , 'nome' => 'Bentrack' , 'created_at' => NOW() , 'updated_at' => NOW() ]
-        ]);
-    }
+            DB::table('impostazione_proprietario_veicolo')->insert([
+                ['id' => '1', 'nome' => 'TOP CAR', 'created_at' => NOW(), 'updated_at' => NOW()],
+                ['id' => '2', 'nome' => 'AUTOMOTIVE', 'created_at' => NOW(), 'updated_at' => NOW()],
+                ['id' => '3', 'nome' => 'MOVIMOB', 'created_at' => NOW(), 'updated_at' => NOW()],
+                ['id' => '4', 'nome' => 'RL2', 'created_at' => NOW(), 'updated_at' => NOW()],
+                ['id' => '5', 'nome' => 'VENETA LOGISTIC', 'created_at' => NOW(), 'updated_at' => NOW()],
+                ['id' => '6', 'nome' => 'SOLUZIONE VEICOLARE', 'created_at' => NOW(), 'updated_at' => NOW()],
+                ['id' => '7', 'nome' => 'B7', 'created_at' => NOW(), 'updated_at' => NOW()],
+                ['id' => '8', 'nome' => 'GAIA78', 'created_at' => NOW(), 'updated_at' => NOW()],
+                ['id' => '9', 'nome' => 'LEASEPLAN', 'created_at' => NOW(), 'updated_at' => NOW()],
+                ['id' => '10', 'nome' => 'DINA BIRIIAC', 'created_at' => NOW(), 'updated_at' => NOW()],
+                ['id' => '11', 'nome' => 'Bentrack', 'created_at' => NOW(), 'updated_at' => NOW()]
+            ]);
+        }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('impostazione_proprietario_veicolo');
-    }
-};
+        /**
+         * Reverse the migrations.
+         */
+        public function down(): void
+        {
+            Schema::dropIfExists('impostazione_proprietario_veicolo');
+        }
+    };
 
     //    export class impostazione_proprietario_veicolo
     //    {
